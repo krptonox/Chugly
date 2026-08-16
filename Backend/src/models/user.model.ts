1727f848d55cb65cd64430726900d2dabba480cd
krptonox <aryanyadav182007@gmail.com>
2026-08-16 19:24:53 +0530

    feat: add bcrypt and mailgen dependencies; implement user model and mail utility
    - Added bcrypt for password hashing and mailgen for email generation in package.json and package-lock.json.
    - Removed old auth.controller.js and mail.js files.
    - Created new auth.controller.ts for handling authentication logic.
    - Implemented user.model.ts to define the user schema with Mongoose.
    - Developed mail.ts utility for sending emails with Mailgen and Nodemailer.

---
+++ Backend/src/models/user.model.ts
@@ -0,0 +1,132 @@
+import mongoose, {
+    Schema,
+    Document,
+    Model,
+} from "mongoose";
+
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+
+
+// --------------------------------------------------
+// Interfaces
+// --------------------------------------------------
+
+interface IAvatar {
+    url: string;
+    localPath: string;
+}
+
+interface ITemporaryToken {
+    unHashedToken: string;
+    hashedToken: string;
+    TokenExpiry: Date;
+}
+
+interface IUser extends Document {
+    avatar: IAvatar;
+
+    username: string;
+    email: string;
+    fullname?: string;
+
+    password: string;
+
+    isEmailVerified: boolean;
+
+    refreshToken?: string | null;
+
+    forgotPasswordToken?: string | null;
+    forgotPasswordTokenExpiry?: Date | null;
+
+    emailVerificationToken?: string | null;
+    emailVerificationTokenExpiry?: Date | null;
+
+}
+
+
+// --------------------------------------------------
+// Schema
+// --------------------------------------------------
+
+const userSchema = new Schema<IUser>(
+    {
+        avatar: {
+            type: {
+                url: {
+                    type: String,
+                    required: true,
+                },
+
+                localPath: {
+                    type: String,
+                    required: true,
+                },
+            },
+
+            default: {
+                url: "https://placehold.co/200x200",
+                localPath: "",
+            },
+        },
+
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+
+        fullname: {
+            type: String,
+            trim: true,
+        },
+
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
+
+        isEmailVerified: {
+            type: Boolean,
+            default: false,
+        },
+
+        refreshToken: {
+            type: String,
+        },
+
+        forgotPasswordToken: {
+            type: String,
+        },
+
+        forgotPasswordTokenExpiry: {
+            type: Date,
+        },
+
+        emailVerificationToken: {
+            type: String,
+        },
+
+        emailVerificationTokenExpiry: {
+            type: Date,
+        },
+    },
+
+    {
+        timestamps: true,
+    }
+);
+
+export const User =  mongoose.model<IUser>("User", userSchema);
---
+++ Backend/src/utils/mail.ts
@@ -0,0 +1,124 @@
+import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+interface SendMailOptions {
+    email: string;
+    subject: string;
+    mailgenContent: Mailgen.Content;
+}
+
+interface MailgenButton {
+    color: string;
+    text: string;
+    link: string;
+}
+
+interface MailgenContent {
+    body: {
+        name: string;
+        intro: string;
+        action: {
+            instruction: string;
+            button: MailgenButton;
+        };
+        outro: string;
+    };
+}
+
+const sendMail = async (options: SendMailOptions): Promise<void> => {
+    const mailGenerator = new Mailgen({
+        theme: "default",
+        product: {
+            name: "Task Manager",
+            link: "http://localhost:3000",
+        },
+    });
+
+    const emailTextual = mailGenerator.generatePlaintext(
+        options.mailgenContent
+    );
+
+    const emailHtml = mailGenerator.generate(
+        options.mailgenContent
+    );
+
+    const transporter = nodemailer.createTransport({
+        host: process.env.MAILTRAP_SMTP_HOST,
+
+        port: Number(process.env.MAILTRAP_SMTP_PORT),
+
+        auth: {
+            user: process.env.MAILTRAP_SMTP_USERNAME,
+            pass: process.env.MAILTRAP_SMTP_PASSWORD,
+        },
+    });
+
+    const mail = {
+        from: "krptonox7@gmail.com",
+        to: options.email,
+        subject: options.subject,
+        text: emailTextual,
+        html: emailHtml,
+    };
+
+    try {
+        await transporter.sendMail(mail);
+    } catch (error) {
+        throw new Error("Error sending email");
+    }
+};
+
+const emailVerficationMailgenContent = (
+    username: string,
+    verificationurl: string
+): MailgenContent => {
+    return {
+        body: {
+            name: username,
+            intro: "Welcome to Dekan! We're very excited to have you on board.",
+            action: {
+                instruction:
+                    "To verify your email, please click the button below:",
+                button: {
+                    color: "#22BC66",
+                    text: "Verify Email",
+                    link: verificationurl,
+                },
+            },
+            outro:
+                "Need help, or have questions? Just reply to this email, we'd love to help.",
+        },
+    };
+};
+
+const forgotPasswordMailgenContent = (
+    username: string,
+    passwordreseturl: string
+): MailgenContent => {
+    return {
+        body: {
+            name: username,
+            intro: "We're resetting your password.",
+            action: {
+                instruction:
+                    "To reset your password, please click the button below:",
+                button: {
+                    color: "#ad1f1f",
+                    text: "Reset Password",
+                    link: passwordreseturl,
+                },
+            },
+            outro:
+                "Need help, or have questions? Just reply to this email, we'd love to help.",
+        },
+    };
+};
+
+export {
+    emailVerficationMailgenContent,
+    forgotPasswordMailgenContent,
+    sendMail,
+};
